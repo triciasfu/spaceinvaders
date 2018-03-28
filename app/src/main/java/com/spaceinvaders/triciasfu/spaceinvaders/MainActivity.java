8beb9b43b632f4ba6b774ae5beb96c363e185ae5
Tricia Fu <triciasfu@triciasfu-macbookpro.roam.corp.google.com>
2018-03-28 15:35:37 -0700

    updated to use only space invaders

--- app/src/main/java/com/spaceinvaders/triciasfu/spaceinvaders/MainActivity.java
+++ app/src/main/java/com/spaceinvaders/triciasfu/spaceinvaders/MainActivity.java
@@ -4,42 +4,107 @@ package com.spaceinvaders.triciasfu.spaceinvaders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
+import com.google.android.things.contrib.driver.button.Button;
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat;
+import com.google.android.things.pio.Gpio;
+
+import java.io.IOException;
+
 import processing.android.CompatUtils;
 import processing.android.PFragment;
-import processing.core.PApplet;
+
+import static processing.core.PConstants.LEFT;
+import static processing.core.PConstants.RIGHT;
+import static processing.core.PConstants.SHIFT;
+
 
 
 public class MainActivity extends AppCompatActivity {
-    private PApplet sketch;
+    private SpaceInvaders b;
+    private static final String RED_PIN_NAME = "GPIO6_IO14";
+    private static final String GREEN_PIN_NAME = "GPIO19_IO14";
+    private static final String BLUE_PIN_NAME = "GPIO26_IO14";
+    private Gpio mButtonGpioRed;
+    private Gpio mButtonGpioGreen;
+    private Gpio mButtonGpioBlue;
+
+    Button buttonA;
+    Button buttonB;
+    Button buttonC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setTheme(R.style.Theme_AppCompat_NoActionBar);
+
+
         FrameLayout frame = new FrameLayout(this);
         frame.setId(CompatUtils.getUniqueViewId());
         setContentView(frame, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT));
 
-        sketch = new Sketch();
-        PFragment fragment = new PFragment(sketch);
+        b = new SpaceInvaders();
+        PFragment fragment = new PFragment(b);
         fragment.setView(frame, this);
+
+        try {
+            buttonA = RainbowHat.openButtonA();
+            buttonB = RainbowHat.openButtonB();
+            buttonC = RainbowHat.openButtonC();
+            buttonA.setOnButtonEventListener(new Button.OnButtonEventListener() {
+                @Override
+                public void onButtonEvent(Button button, boolean pressed) {
+                    if (pressed) {
+                        b.keyPressed = true;
+                        b.keyCode = LEFT;
+                    } else {
+                        b.keyPressed = false;
+                    }
+                }
+            });
+            buttonB.setOnButtonEventListener(new Button.OnButtonEventListener() {
+                @Override
+                public void onButtonEvent(Button button, boolean pressed) {
+                    if (pressed) {
+                        b.keyPressed = true;
+                        b.keyCode = SHIFT;
+                    } else {
+                        b.keyPressed = false;
+                    }
+                }
+            });
+            buttonC.setOnButtonEventListener(new Button.OnButtonEventListener() {
+                @Override
+                public void onButtonEvent(Button button, boolean pressed) {
+                    if (pressed) {
+                        b.keyPressed = true;
+                        b.keyCode = RIGHT;
+                    } else {
+                        b.keyPressed = false;
+                    }
+                }
+            });
+        } catch (IOException e) {
+            Log.d("I AHATE AMDK:", "SUCKS");
+        }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        if (sketch != null) {
-            sketch.onRequestPermissionsResult(
+        if (b != null) {
+            b.onRequestPermissionsResult(
                     requestCode, permissions, grantResults);
         }
     }
 
     @Override
     public void onNewIntent(Intent intent) {
-        if (sketch != null) {
-            sketch.onNewIntent(intent);
+        if (b != null) {
+            b.onNewIntent(intent);
         }
     }
 }
--- app/src/main/java/com/spaceinvaders/triciasfu/spaceinvaders/SpaceInvaders.java
+++ app/src/main/java/com/spaceinvaders/triciasfu/spaceinvaders/SpaceInvaders.java
@@ -1,95 +1,180 @@
 package com.spaceinvaders.triciasfu.spaceinvaders;
 
-import android.widget.AbsListView;
-import android.widget.Button;
-import android.widget.FrameLayout;
-
 import java.util.ArrayList;
 
 import processing.core.PApplet;
-import processing.core.PImage;
 
 /**
- * Created by triciasfu on 3/13/18.
+ * Created by triciasfu on 3/25/18.
  */
 
-public class SpaceInvaders extends PApplet{
-
+public class SpaceInvaders extends PApplet {
+    int pixelsize = 4;
+    int gridsize  = pixelsize * 7 + 5;
     Player player;
+    ArrayList<Enemy> enemies = new ArrayList();
     ArrayList<Bullet> bullets = new ArrayList();
-    ArrayList shields = new ArrayList();
-    int shieldNum = 4;
-    int invaderCols = 10;
-    int invaderRows = 5;
+    int direction = 1;
+    boolean incy = false;
 
-    AlienField alienField;
-    PImage invaderSprite;
-
-    Button restartButton;
-
-    //Sound
-//    var shootSound;
-    ArrayList move = new ArrayList();
-//    var invaderDie;
-//    var hitSound;
-
-    @Override
-    public void setup() {
-
-        player = new Player();
-
-        alienField = new AlienField(width/2, height/4, invaderRows, invaderCols, invaderSprite);
-        alienField.init();
-
-        for(int i = 0; i < shieldNum; i++) {
-            shields.push(new Shield(width*0.1+(width*0.8)/(shieldNum-1)*i, height-150));
-            shields.get(i).init();
-        }
+    public void settings() {
+        size(800, 500);
     }
 
-    @Override
+    public void setup() {
+        background(0);
+        noStroke();
+        fill(255);
+        player = new Player();
+        createEnemies();
+    }
+
     public void draw() {
         background(0);
 
-        if(!player.gameOver && !alienField.gameOver && !alienField.win) {
-            for(int i = bullets.size()-1; i >= 0; i--) {
-                bullets.get(i).update();
-                bullets.get(i).render();
+        player.draw();
 
-                if(bullets.get(i).despawn()) {
-                    bullets = (ArrayList) bullets.subList(i,1);
+        for (int i = 0; i < bullets.size(); i++) {
+            Bullet bullet = (Bullet) bullets.get(i);
+            bullet.draw();
+        }
+
+        for (int i = 0; i < enemies.size(); i++) {
+            Enemy enemy = (Enemy) enemies.get(i);
+            if (enemy.outside() == true) {
+                direction *= (-1);
+                incy = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < enemies.size(); i++) {
+            Enemy enemy = (Enemy) enemies.get(i);
+            if (!enemy.alive()) {
+                enemies.remove(i);
+            }
+            else {
+                enemy.draw();
+            }
+        }
+
+        incy = false;
+    }
+
+    public void createEnemies() {
+        for (int i = 0; i < width/gridsize/2; i++) {
+            for (int j = 0; j <= 5; j++) {
+                enemies.add(new Enemy(i*gridsize, j*gridsize));
+            }
+        }
+    }
+
+    public class SpaceShip {
+        int x, y;
+        String sprite[];
+
+        void draw() {
+            updateObj();
+            drawSprite(x, y);
+        }
+
+        void drawSprite(int xpos, int ypos) {
+            for (int i = 0; i < sprite.length; i++) {
+                String row = (String) sprite[i];
+
+                for (int j = 0; j < row.length(); j++) {
+                    if (row.charAt(j) == '1')
+                        rect(xpos+(j * pixelsize), ypos+(i * pixelsize), pixelsize, pixelsize);
+                }
+            }
+        }
+
+        void updateObj() {
+        }
+    }
+
+    public class Player extends SpaceShip {
+        boolean canShoot = true;
+        int shootdelay = 0;
+
+        Player() {
+            x = width/gridsize/2;
+            y = height - (10 * pixelsize);
+            sprite    = new String[5];
+            sprite[0] = "0010100";
+            sprite[1] = "0110110";
+            sprite[2] = "1111111";
+            sprite[3] = "1111111";
+            sprite[4] = "0111110";
+        }
+
+        void updateObj() {
+
+            if (keyPressed && keyCode == LEFT) x -= 10;
+            if (keyPressed && keyCode == RIGHT) x += 10;
+            if (keyPressed && keyCode == SHIFT && canShoot) {
+                bullets.add(new Bullet(x, y));
+                canShoot = false;
+                shootdelay = 0;
+            }
+
+            shootdelay++;
+            if (shootdelay >= 5) {
+                canShoot = true;
+            }
+        }
+    }
+
+    public class Enemy extends SpaceShip {
+        Enemy(int xpos, int ypos) {
+            x = xpos;
+            y = ypos;
+            sprite    = new String[5];
+            sprite[0] = "1011101";
+            sprite[1] = "0101010";
+            sprite[2] = "1111111";
+            sprite[3] = "0101010";
+            sprite[4] = "1000001";
+        }
+
+        void updateObj() {
+            if (frameCount%30 == 0) x += direction * gridsize;
+            if (incy == true) y += gridsize / 2;
+        }
+
+        boolean alive() {
+            for (int i = 0; i < bullets.size(); i++) {
+                Bullet bullet = (Bullet) bullets.get(i);
+                if (bullet.x > x && bullet.x < x + 7 * pixelsize + 5 && bullet.y > y && bullet.y < y + 5 * pixelsize) {
+                    bullets.remove(i);
+                    return false;
                 }
             }
 
-            for(int i = 0; i < shieldNum; i++) {
-                shields.get(i).render();
-                shields.get(i).update(bullets);
-            }
-
-            alienField.update();
-            player.score += alienField.alienHit(bullets);
-            alienField.alienShoot(bullets);
-            alienField.render();
-
-            player.render();
-            player.update();
-            player.hit(bullets);
-        } else if(!alienField.win && (player.gameOver || alienField.gameOver)) {
-            textSize(width/10);
-            fill(255);
-            textAlign(CENTER, CENTER);
-            text("Game Over!", width/2, height/2-width/10);
-            text("Score: " + player.score, width/2, height/2);
-//            restartButton.show();
-        } else if(alienField.win) {
-            textSize(width/10);
-            fill(255);
-            textAlign(CENTER, CENTER);
-            text("You Win", width/2, height/2-width/10);
-            text("Score: " + player.score, width/2, height/2);
-//            restartButton.show();
+            return true;
         }
 
+        boolean outside() {
+            if (x + (direction*gridsize) < 0 || x + (direction*gridsize) > width - gridsize) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
 
+    public class Bullet {
+        int x, y;
+
+        Bullet(int xpos, int ypos) {
+            x = xpos + gridsize/2 - 4;
+            y = ypos;
+        }
+
+        void draw() {
+            rect(x, y, pixelsize, pixelsize);
+            y -= pixelsize*4;
+        }
     }
 }
